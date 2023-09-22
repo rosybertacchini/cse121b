3ff0b1ca64af5887f1586efffd7fa63193a6a8d0
rosybertacchini <rosybertacchini@gmail.com>
2023-09-21 20:24:30 -0700

    w2 prep 1-5

--- scripts/w02-task.js
+++ scripts/w02-task.js
@@ -3,29 +3,42 @@
 /* Step 1 - Setup type tasks - no code required */
 
 /* Step 2 - Variables */
-/* ------------------------------------------------------------------------ */
 let fullName = "Rosy Bertacchini";
-let currentYear =  "";
-let profilePicture = "";
-const myPicture  = "images/RosaMaZamarronMedrano.jpg";
+let currentYear = "";
+
+//*
+const profilePicture = "https://github.com/rosybertacchini/cse121b/images/Yo_RosyBertacchini-1.png";
+
 
 /* Step 3 - Element Variables */
-/* ------------------------------------------------------------------------ */
 const nameElemet = document.getElementById("name");
 const foodElemet = document.getElementById("food");
 const yearElement = document.querySelector("#year");
-const imageElement = document.getElementById("myPicture");
 
 /* Step 4 - Adding Content */
-/* ------------------------------------------------------------------------ */
-nameElemet.innerHTML = `<strong> ${fullName} </strong>`
+nameElemet.innerHTML = `<strong> ${fullName} </strong>`;
 yearElement.textContent = "2023";
-imageElement.setAttribute("scr",myPicture);
+
+
+//*
+const imageElement = document.querySelector("img");
+imageElement.setAttribute('scr', profilePicture); 
+imageElement.setAttribute('alt', 'Profile image of Rosy Bertacchini'); 
 
 
 
 /* Step 5 - Array */
-/* ------------------------------------------------------------------------ */
+let favoriteFoods= ['avocado toast', 'asparagus', 'artichokes', 'marlin fish','mango'];
+
+let i = 0;
+let favoriteFood = "";
+while (i <favoriteFoods.length) {
+    favoriteFood += favoriteFoods[i]+" / ";
+    i++;
+}
+
+foodElemet.innerHTML = `${favoriteFood}`;
+
 
 
 
