5ec9645a0a23682b7a1ada99f27419700c63f97d
rosybertacchini <rosybertacchini@gmail.com>
2023-10-12 20:48:46 -0700

    W05 pictures

--- scripts/w05-task.js
+++ scripts/w05-task.js
@@ -10,21 +10,35 @@ const displayTemples = (temples) => {
 
   temples.forEach((temple) => {
 
-      h3Element = document.createElement("h1");
+      h3Element = document.createElement("h3");
       h3Element.innerHTML = `${temple.templeName}`;
+      h3Element.style.textAlign = "center";
+      h3Element.style.position = "relative";
+      h3Element.style.bottom = "15px";
+
 
       const articleElement = document.createElement('article');
+      articleElement.style.backgroundColor = "#c7d7f2";
+      articleElement.style.padding = "30px";
+      articleElement.style.margin = "15px";
+      
+
+
       const imageElement   = document.createElement('img');
 
       imageElement.src =  `${temple.imageUrl}`;
+      
       // imageElement.setAttribute("scr", `${temple.imageUrl}`);  // did not work like this
       imageElement.alt = `${temple.location}`;
       imageElement.width = "200";
       imageElement.height = "200";
+      imageElement.style.boxShadow = "0px 0px 10px 10px";
+
 
       templeElement.appendChild(articleElement).appendChild(h3Element);
       templeElement.appendChild(articleElement).appendChild(imageElement);
       templeElement.appendChild(articleElement);
+      
 
       //console.log(temple.imageUrl);
   });
@@ -44,7 +58,7 @@ const getTemples = async () => {
 
 const reset = () => {
   templeElement.replaceChildren();
-  templeList = {};
+
 };
 
 // Anotehr way to filter, no so optimaze 
@@ -61,6 +75,8 @@ const sortby = (temples) => {
   
 
   const filter = document.getElementById("sortBy").value;
+  const sortByElement = document.getElementById("sortBy");
+  sortByElement.style.position = "absolute;";
 
   switch (filter) {
     case "utah":
